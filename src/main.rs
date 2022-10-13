72a362951aa5dfafcd942b606e1b49ea69925433
Tim Van Wassenhove <tim@timvw.be>
2022-10-13 14:58:55 +0200

    feat: add test to verify that running the app works as expected

--- src/main.rs
+++ src/main.rs
@@ -40,3 +40,32 @@ fn set_aws_profile_when_needed(args: &Args) {
         env::set_var("AWS_PROFILE", aws_profile);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_cmd::prelude::*;
+    use std::process::Command;
+    use predicates::prelude::*;
+    use datafusion::common::DataFusionError;
+
+    #[tokio::test]
+    async fn run_without_file_exits_with_usage() -> Result<()> {
+        let mut cmd = Command::cargo_bin("qv")
+            .map_err(|e| DataFusionError::External(Box::new(e)))?;
+        cmd.assert().failure().stderr(predicate::str::contains("Usage: qv <PATH>"));
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn run_with_local_avro_file() -> Result<()> {
+        let mut cmd = Command::cargo_bin("qv")
+            .map_err(|e| DataFusionError::External(Box::new(e)))?;
+        //let cmd = cmd.arg("/Users/timvw/src/github/qv/testing/data/avro/alltypes_plain.avro");
+        let cmd = cmd.arg("./testing/data/avro/alltypes_plain.avro");
+        cmd.assert().success()
+            .stdout(predicate::str::contains("| id | bool_col | tinyint_col | smallint_col | int_col | bigint_col | float_col | double_col | date_string_col  | string_col | timestamp_col       |"))
+            .stdout(predicate::str::contains("| 4  | true     | 0           | 0            | 0       | 0          | 0         | 0          | 30332f30312f3039 | 30         | 2009-03-01 00:00:00 |"));
+        Ok(())
+    }
+}
